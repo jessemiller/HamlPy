16d999ed6a257a2a54832e21609d6fec013bc7e5
Jesse Miller <millerjesse@gmail.com>
2010-05-20 16:45:11 -0400

    Made hamlpy executable

--- hamlpy/hamlpy.py
+++ hamlpy/hamlpy.py
@@ -13,4 +13,23 @@ class Compiler:
             rootNode.addNode(hamlNode)
         return rootNode.render()
     
+if __name__ == '__main__':
+    import sys
+    import codecs
+    
+    if (len(sys.argv) < 2):
+        print "Specify the input file as the first argument."
+    else: 
+        inFile = sys.argv[1]
+        hamlLines = codecs.open(inFile, 'r', encoding='utf-8').read().splitlines()
+
+        compiler = Compiler()
+        output = compiler.processLines(hamlLines)
+        
+        if (len(sys.argv) == 3):
+            outFile = open(sys.argv[2], 'w')
+            outFile.write(output)
+        else:
+            print output
+    
 
