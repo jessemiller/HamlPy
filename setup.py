0e1b2920ff78037e1ce2ee563d5cb0bb3e238999
Jesse Miller <millerjesse@gmail.com>
2010-05-20 16:37:52 -0400

    Added the setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hamlpy',
+      version = '0.1',
+      packages = ['hamlpy'],
+      author = 'Jesse Miller',
+      author_email = 'millerjesse@gmail.com',
+      description = 'HAML like syntax for Django templates',
+      keywords = 'haml django converter',
+      url = 'http://github.com/jessemiller/HamlPy'
+    )
