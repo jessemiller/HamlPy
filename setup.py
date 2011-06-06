fdb27db8f52c206f8592fbf35a05251db4d4fff4
jessemiller <millerjesse@gmail.com>
2011-06-05 22:28:03 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hamlpy',
-      version = '0.1',
+      version = '0.2',
       packages = ['hamlpy'],
       author = 'Jesse Miller',
       author_email = 'millerjesse@gmail.com',
