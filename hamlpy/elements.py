63e01482938d97fc51fac105768d16234aec9088
Mehershad D <mdd3sum@gmail.com>
2012-01-06 00:19:36 -0800

    Fixing issue with empty attribute values like 'itemscope'. Please refer to my discussion on Issue #58 https://github.com/jessemiller/HamlPy/issues/58

--- hamlpy/elements.py
+++ hamlpy/elements.py
@@ -79,6 +79,8 @@ class Element(object):
                 attributes_dict = eval(attribute_dict_string)
                 for k, v in attributes_dict.items():
                     if k != 'id' and k != 'class':
+                        if v.lower() in ("yes", "true", "t", "1"):
+                            self.attributes += "%s " % (k,)
                         if isinstance(v, int) or isinstance(v, float):
                             self.attributes += "%s='%s' " % (k, v)
                         else:
