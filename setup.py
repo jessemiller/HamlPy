ebddacfd6ac50a570d16ddf966104503f22c4151
Jesse Miller <millerjesse@gmail.com>
2012-02-21 15:39:18 -0800

    Roll version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hamlpy',
-      version = '0.6',
+      version = '0.7',
       download_url = 'git@github.com:jessemiller/HamlPy.git',
       packages = ['hamlpy'],
       author = 'Jesse Miller',
