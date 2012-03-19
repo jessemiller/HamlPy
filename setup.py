0c2929f6c30932659cecdf758682c5a363ffa766
Jesse Miller <millerjesse@gmail.com>
2012-03-18 18:17:44 -0700

    roll version

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 
+# Note to Jesse - only push sdist to PyPi, bdist seems to always break pip installer
 setup(name='hamlpy',
-      version = '0.80.1',
+      version = '0.80.3',
       download_url = 'git@github.com:jessemiller/HamlPy.git',
       packages = ['hamlpy'],
       author = 'Jesse Miller',
