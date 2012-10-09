358b21e553e30453a6018343a17d18ad125e6461
Richard Eames <reames@asymmetricventures.com>
2012-10-09 17:55:43 -0600

    - Add a test for whitespace after attribute key

--- hamlpy/test/regression.py
+++ hamlpy/test/regression.py
@@ -17,3 +17,9 @@ class RegressionTest(unittest.TestCase):
         result = hamlParser.process(haml)
         eq_(html, result.strip())
         
+    def test_whitespace_after_attribute_key(self):
+        haml = '%form{id : "myform"}'
+        html = "<form id='myform'></form>"
+        hamlParser = hamlpy.Compiler()
+        result = hamlParser.process(haml)
+        eq_(html, result.strip())
