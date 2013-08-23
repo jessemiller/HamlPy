62bc8cb5e3c12dc8193bf74eb5cae13b068344be
Andrew Cordery <cordery@gmail.com>
2013-08-22 23:30:42 -0400

    Modified templatize to ignore non-haml files (fix)

--- hamlpy/templatize.py
+++ hamlpy/templatize.py
@@ -1,20 +1,20 @@
 """
 This module decorates the django templatize function to parse haml templates
 before the translation utility extracts tags from it.
- 
+
 --Modified to ignore non-haml files.
 """
- 
+
 try:
     from django.utils.translation import trans_real
     _django_available = True
 except ImportError, e:
     _django_available = False
- 
+
 import hamlpy
 import os
- 
- 
+
+
 def decorate_templatize(func):
     def templatize(src, origin=None):
         #if the template has no origin file then do not attempt to parse it with haml
@@ -26,6 +26,6 @@ def decorate_templatize(func):
                 src = html.encode('utf-8')
         return func(src, origin)
     return templatize
- 
+
 if _django_available:
     trans_real.templatize = decorate_templatize(trans_real.templatize)
