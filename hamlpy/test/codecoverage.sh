26ceefd44f7e49f244e25bf7613e07a2e92c835c
Niels van Huijstee <niels.vanhuijstee@cyso.com>
2015-12-18 11:16:34 +0100

    Make use of setup.py for tests

--- hamlpy/test/codecoverage.sh
+++
@@ -1,1 +0,0 @@
-nosetests *.py --with-coverage --cover-html --cover-inclusive --cover-package=hamlpy.*
--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 from setuptools import setup
 
 # Note to Jesse - only push sdist to PyPi, bdist seems to always break pip installer
-setup(name='hamlpy',
-      version = '0.82.2',
-      download_url = 'git@github.com:jessemiller/HamlPy.git',
-      packages = ['hamlpy', 'hamlpy.template'],
-      author = 'Jesse Miller',
-      author_email = 'millerjesse@gmail.com',
-      description = 'HAML like syntax for Django templates',
-      keywords = 'haml django converter',
-      url = 'http://github.com/jessemiller/HamlPy',
-      license = 'MIT',
-      install_requires = [
-      ],
-      entry_points = {
-          'console_scripts' : ['hamlpy = hamlpy.hamlpy:convert_files',
-                               'hamlpy-watcher = hamlpy.hamlpy_watcher:watch_folder']
-      }
-    )
+setup(
+    name='hamlpy',
+    version='0.82.2',
+    download_url='git@github.com:jessemiller/HamlPy.git',
+    packages=['hamlpy', 'hamlpy.template'],
+    author='Jesse Miller',
+    author_email='millerjesse@gmail.com',
+    description='HAML like syntax for Django templates',
+    keywords='haml django converter',
+    url='http://github.com/jessemiller/HamlPy',
+    license='MIT',
+    test_suite='hamlpy.test',
+    install_requires=[],
+    setup_requires=['mock'],
+    entry_points={
+        'console_scripts':
+            ['hamlpy = hamlpy.hamlpy:convert_files',
+             'hamlpy-watcher = hamlpy.hamlpy_watcher:watch_folder']
+    }
+)
