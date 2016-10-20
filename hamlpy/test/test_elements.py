93d2147eb014c5e22dd0069ca6503278d2c455c6
Rowan Seymour <rowanseymour@gmail.com>
2016-10-20 13:56:05 +0200

    Tidy up testing modules

--- hamlpy/test/test_elements.py
+++ hamlpy/test/test_elements.py
@@ -8,93 +8,93 @@ from nose.tools import eq_
 
 class ElementTest(unittest.TestCase):
 
-        def test_escape_attribute_quotes(self):
-            sut = Element('')
+    def test_escape_attribute_quotes(self):
+        sut = Element('')
 
-            s1 = sut._escape_attribute_quotes('''{% url 'blah' %}''')
-            eq_(s1,'''{% url 'blah' %}''')
+        s1 = sut._escape_attribute_quotes('''{% url 'blah' %}''')
+        eq_(s1, '''{% url 'blah' %}''')
 
-            s2 = sut._escape_attribute_quotes('''blah's blah''s {% url 'blah' %} blah's blah''s''')
-            eq_(s2,r"blah\'s blah\'\'s {% url 'blah' %} blah\'s blah\'\'s")
+        s2 = sut._escape_attribute_quotes('''blah's blah''s {% url 'blah' %} blah's blah''s''')
+        eq_(s2, r"blah\'s blah\'\'s {% url 'blah' %} blah\'s blah\'\'s")
 
-        def test_pulls_tag_name_off_front(self):
-            sut = Element('%div.class')
-            eq_(sut.tag, 'div')
-            
-        def test_default_tag_is_div(self):
-            sut = Element('.class#id')
-            eq_(sut.tag, 'div')
-            
-        def test_parses_id(self):
-            sut = Element('%div#someId.someClass')
-            eq_(sut.id, 'someId')
-            
-            sut = Element('#someId.someClass')
-            eq_(sut.id, 'someId')
-            
-        def test_no_id_gives_empty_string(self):
-            sut = Element('%div.someClass')
-            eq_(sut.id, '')
-        
-        def test_parses_class(self):
-            sut = Element('%div#someId.someClass')
-            eq_(sut.classes, 'someClass')
-            
-        def test_properly_parses_multiple_classes(self):
-            sut = Element('%div#someId.someClass.anotherClass')
-            eq_(sut.classes, 'someClass anotherClass')
-            
-        def test_no_class_gives_empty_string(self):
-            sut = Element('%div#someId')
-            eq_(sut.classes, '')
-            
-        def test_attribute_dictionary_properly_parses(self):
-            sut = Element("%html{'xmlns':'http://www.w3.org/1999/xhtml', 'xml:lang':'en', 'lang':'en'}")
-            assert "xmlns='http://www.w3.org/1999/xhtml'" in sut.attributes
-            assert "xml:lang='en'" in sut.attributes
-            assert "lang='en'" in sut.attributes
+    def test_pulls_tag_name_off_front(self):
+        sut = Element('%div.class')
+        eq_(sut.tag, 'div')
 
-        def test_attribute_merges_classes_properly(self):
-            sut = Element("%div.someClass.anotherClass{'class':'hello'}")
-            assert 'someClass' in sut.classes
-            assert 'anotherClass' in sut.classes
-            assert 'hello' in sut.classes
-            
-        def test_attribute_merges_ids_properly(self):
-            sut = Element("%div#someId{'id':'hello'}")
-            eq_(sut.id, 'someId_hello')
-            
-        def test_can_use_arrays_for_id_in_attributes(self):
-            sut = Element("%div#someId{'id':['more', 'andMore']}")
-            eq_(sut.id, 'someId_more_andMore')
-        
-        def test_self_closes_a_self_closing_tag(self):
-            sut = Element(r"%br")
-            assert sut.self_close
-            
-        def test_does_not_close_a_non_self_closing_tag(self):
-            sut = Element("%div")
-            assert sut.self_close == False
-            
-        def test_can_close_a_non_self_closing_tag(self):
-            sut = Element("%div/")
-            assert sut.self_close
-            
-        def test_properly_detects_django_tag(self):
-            sut = Element("%div= $someVariable")
-            assert sut.django_variable
-            
-        def test_knows_when_its_not_django_tag(self):
-            sut = Element("%div Some Text")
-            assert sut.django_variable == False
-            
-        def test_grabs_inline_tag_content(self):
-            sut = Element("%div Some Text")
-            eq_(sut.inline_content, 'Some Text')
-            
-        def test_multiline_attributes(self):
-            sut = Element("""%link{'rel': 'stylesheet', 'type': 'text/css',
-                'href': '/long/url/to/stylesheet/resource.css'}""")
-            assert "href='/long/url/to/stylesheet/resource.css'" in sut.attributes
-            assert "type='text/css'" in sut.attributes
-            assert "rel='stylesheet'" in sut.attributes
+    def test_default_tag_is_div(self):
+        sut = Element('.class#id')
+        eq_(sut.tag, 'div')
+
+    def test_parses_id(self):
+        sut = Element('%div#someId.someClass')
+        eq_(sut.id, 'someId')
+
+        sut = Element('#someId.someClass')
+        eq_(sut.id, 'someId')
+
+    def test_no_id_gives_empty_string(self):
+        sut = Element('%div.someClass')
+        eq_(sut.id, '')
+
+    def test_parses_class(self):
+        sut = Element('%div#someId.someClass')
+        eq_(sut.classes, 'someClass')
+
+    def test_properly_parses_multiple_classes(self):
+        sut = Element('%div#someId.someClass.anotherClass')
+        eq_(sut.classes, 'someClass anotherClass')
+
+    def test_no_class_gives_empty_string(self):
+        sut = Element('%div#someId')
+        eq_(sut.classes, '')
+
+    def test_attribute_dictionary_properly_parses(self):
+        sut = Element("%html{'xmlns':'http://www.w3.org/1999/xhtml', 'xml:lang':'en', 'lang':'en'}")
+        assert "xmlns='http://www.w3.org/1999/xhtml'" in sut.attributes
+        assert "xml:lang='en'" in sut.attributes
+        assert "lang='en'" in sut.attributes
+
+    def test_attribute_merges_classes_properly(self):
+        sut = Element("%div.someClass.anotherClass{'class':'hello'}")
+        assert 'someClass' in sut.classes
+        assert 'anotherClass' in sut.classes
+        assert 'hello' in sut.classes
+
+    def test_attribute_merges_ids_properly(self):
+        sut = Element("%div#someId{'id':'hello'}")
+        eq_(sut.id, 'someId_hello')
+
+    def test_can_use_arrays_for_id_in_attributes(self):
+        sut = Element("%div#someId{'id':['more', 'andMore']}")
+        eq_(sut.id, 'someId_more_andMore')
+
+    def test_self_closes_a_self_closing_tag(self):
+        sut = Element(r"%br")
+        assert sut.self_close
+
+    def test_does_not_close_a_non_self_closing_tag(self):
+        sut = Element("%div")
+        assert sut.self_close == False
+
+    def test_can_close_a_non_self_closing_tag(self):
+        sut = Element("%div/")
+        assert sut.self_close
+
+    def test_properly_detects_django_tag(self):
+        sut = Element("%div= $someVariable")
+        assert sut.django_variable
+
+    def test_knows_when_its_not_django_tag(self):
+        sut = Element("%div Some Text")
+        assert sut.django_variable == False
+
+    def test_grabs_inline_tag_content(self):
+        sut = Element("%div Some Text")
+        eq_(sut.inline_content, 'Some Text')
+
+    def test_multiline_attributes(self):
+        sut = Element("""%link{'rel': 'stylesheet', 'type': 'text/css',
+            'href': '/long/url/to/stylesheet/resource.css'}""")
+        assert "href='/long/url/to/stylesheet/resource.css'" in sut.attributes
+        assert "type='text/css'" in sut.attributes
+        assert "rel='stylesheet'" in sut.attributes
---
+++ hamlpy/test/test_templates.py
@@ -0,0 +1,127 @@
+from __future__ import print_function, unicode_literals
+
+import os
+import codecs
+import time
+import unittest
+
+from hamlpy import hamlpy
+from os import listdir, path
+
+
+TEMPLATE_DIRECTORY = '/templates/'
+TEMPLATE_EXTENSION = '.hamlpy'
+
+
+class TemplateCheck(object):
+    def __init__(self, name, haml, html):
+        self.name = name
+        self.haml = haml
+        self.expected_html = html.replace('\r', '')  # ignore line ending differences
+        self.actual_html = None
+
+    @classmethod
+    def load_all(cls):
+        directory = os.path.dirname(__file__) + TEMPLATE_DIRECTORY
+        tests = []
+
+        # load all test files
+        for f in listdir(directory):
+            haml_path = path.join(directory, f)
+            if haml_path.endswith(TEMPLATE_EXTENSION):
+                html_path = path.splitext(haml_path)[0] + '.html'
+
+                haml_lines = codecs.open(haml_path, encoding='utf-8').readlines()
+                html = open(html_path, 'r').read()
+
+                tests.append(TemplateCheck(haml_path, haml_lines, html))
+
+        return tests
+
+    def run(self):
+        compiler = hamlpy.Compiler()
+        parsed = compiler.process_lines(self.haml)
+
+        # ignore line ending differences
+        self.actual_html = parsed.replace('\r', '')
+
+    def passed(self):
+        return self.actual_html == self.expected_html
+
+
+class TemplateCompareTest(unittest.TestCase):
+
+    def test_templates(self):
+        tests = TemplateCheck.load_all()
+
+        for test in tests:
+            print('Template test: ' + test.name)
+            test.run()
+
+            if not test.passed():
+                print('\nHTML (actual): ')
+                print('\n'.join(["%d. %s" % (i + 1, l) for i, l in enumerate(test.actual_html.split('\n'))]))
+                self._print_diff(test.actual_html, test.expected_html)
+
+                self.fail()
+
+    @staticmethod
+    def _print_diff(s1, s2):
+        if len(s1) > len(s2):
+            shorter = s2
+        else:
+            shorter = s1
+
+        line = 1
+        col = 1
+
+        for i, _ in enumerate(shorter):
+            if len(shorter) <= i + 1:
+                print('Ran out of characters to compare!')
+                print('Actual len=%d' % len(s1))
+                print('Expected len=%d' % len(s2))
+                break
+            if s1[i] != s2[i]:
+                print('Difference begins at line', line, 'column', col)
+                actual_line = s1.splitlines()[line - 1]
+                expected_line = s2.splitlines()[line - 1]
+                print('HTML (actual, len=%2d)   : %s' % (len(actual_line), actual_line))
+                print('HTML (expected, len=%2d) : %s' % (len(expected_line), expected_line))
+                print('Character code (actual)  : %d (%s)' % (ord(s1[i]), s1[i]))
+                print('Character code (expected): %d (%s)' % (ord(s2[i]), s2[i]))
+                break
+
+            if shorter[i] == '\n':
+                line += 1
+                col = 1
+            else:
+                col += 1
+        else:
+            print("No Difference Found")
+
+
+def performance_test(num_runs):
+    """
+    Performance test which evaluates all the testing templates a given number of times
+    """
+    print("Loading all templates...")
+
+    template_tests = TemplateCheck.load_all()
+
+    print("Running templates tests...")
+
+    times = []
+
+    for r in range(num_runs):
+        start = time.time()
+
+        for test in template_tests:
+            test.run()
+
+        times.append(time.time() - start)
+
+    print("Ran template tests %d times in %.2f seconds (average = %.3f secs)"
+          % (num_runs, sum(times), sum(times) / float(num_runs)))
+
+if __name__ == '__main__':
+    performance_test(500)
