87ffdcfc6eab3dc7ae0b93d1ddc11cf2b7c28876
Rowan Seymour <rowanseymour@gmail.com>
2017-03-17 13:24:39 +0200

    Make hamlpy module a Django app which can be loaded to ensure that templatize is patched

--- hamlpy/__init__.py
+++ hamlpy/__init__.py
@@ -1,6 +1,18 @@
 from __future__ import unicode_literals
 
+from django.apps import AppConfig
+
 __version__ = '1.0.1'
 
-
 HAML_EXTENSIONS = ('haml', 'hamlpy')
+
+
+class Config(AppConfig):
+    name = 'hamlpy'
+
+    def ready(self):
+        # patch Django's templatize method
+        from .template import templatize  # noqa
+
+
+default_app_config = 'hamlpy.Config'
