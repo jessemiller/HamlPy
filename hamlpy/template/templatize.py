87c3fee9c5bd100f143f483a21424784b78ef380
Rowan Seymour <rowanseymour@gmail.com>
2017-05-29 16:33:20 +0200

    Fix for django 1.11 makemessages

--- hamlpy/template/templatize.py
+++ hamlpy/template/templatize.py
@@ -14,7 +14,7 @@ from hamlpy.compiler import Compiler
 
 
 def patch_templatize(func):
-    def templatize(src, origin=None):
+    def templatize(src, origin=None, charset='utf-8'):
         # if the template has no origin then don't attempt to convert it because we don't know if it's Haml
         if origin:
             extension = os.path.splitext(origin)[1][1:].lower()
