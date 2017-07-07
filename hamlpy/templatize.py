ff5ebd1748ece1f012c037a838cb470f78a6ed24
a1s <alex@gorka.lv>
2017-07-07 14:09:56 +0300

    Compatibility fix for Django v1.11: the source for translation.templatize() is Unicode, not bytestring

--- hamlpy/templatize.py
+++ hamlpy/templatize.py
@@ -16,15 +16,14 @@ import os
 
 
 def decorate_templatize(func):
-    def templatize(src, origin=None):
+    def templatize(src, origin=None, **kwargs):
         #if the template has no origin file then do not attempt to parse it with haml
         if origin:
             #if the template has a source file, then only parse it if it is haml
             if os.path.splitext(origin)[1].lower() in ['.'+x.lower() for x in hamlpy.VALID_EXTENSIONS]:
                 hamlParser = hamlpy.Compiler()
-                html = hamlParser.process(src.decode('utf-8'))
-                src = html.encode('utf-8')
-        return func(src, origin)
+                src = hamlParser.process(src)
+        return func(src, origin=origin, **kwargs)
     return templatize
 
 if _django_available:
